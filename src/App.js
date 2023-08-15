3c97c099e514e6b0bbc363a8d828bad4fe10c3c4
Zachary Honey <zhoney466@gmail.com>
2023-08-15 11:56:41 -0700

    Working on mvp items

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
 
+import ProductContext from './contexts/ProductContext';
+import CartContext from './contexts/CartContext';
+
 // Components
 import Navigation from './components/Navigation';
 import Products from './components/Products';
@@ -11,22 +14,28 @@ function App() {
 	const [products] = useState(data);
 	const [cart, setCart] = useState([]);
 
+
+
 	const addItem = item => {
-		// add the given item to the cart
+		setCart({ ...cart, item });
 	};
 
 	return (
 		<div className="App">
-			<Navigation cart={cart} />
+			<CartContext.Provider value={cart}>X
+				<Navigation />
 
-			{/* Routes */}
-			<Route exact path="/">
-				<Products products={products} addItem={addItem} />
-			</Route>
+				{/* Routes */}
+				<ProductContext.Provider value={{ products, addItem }}>
+					<Route exact path="/">
+						<Products />
+					</Route>
+				</ProductContext.Provider>
+				<Route path="/cart">
+					<ShoppingCart />
+				</Route>
+			</CartContext.Provider>
 
-			<Route path="/cart">
-				<ShoppingCart cart={cart} />
-			</Route>
 		</div>
 	);
 }
